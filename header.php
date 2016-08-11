731325358ebba651135aefc865b73e28b116ec2b
Mantovani Gabriele <gabriele.mantovani92@gmail.com>
2016-08-11 17:56:31 +0200

    Bongu Logo

--- header.php
+++ header.php
@@ -49,10 +49,11 @@
 				<div id="inner-header" class="wrap cf">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<div class="svg-Logo_Bongu" id="logo"></div>
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
-					<?php // bloginfo('description'); ?>
+					<?php //bloginfo('description'); ?>
+					<p id="titleDescription">Just another Wordpress StarterKit Theme</p>
 
 
 					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
